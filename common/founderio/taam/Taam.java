fafa9da80e429cfa723f4c0cc0c0d634f23cfd0c
founderio <oliver.kahrmann@gmail.com>
2014-01-14 22:52:09 +0100

    Mod Metadata, added Icon to Creative Tab

--- common/founderio/taam/Taam.java
+++ common/founderio/taam/Taam.java
@@ -8,6 +8,10 @@ public final class Taam {
 	public static final String MOD_ID = "taam";
 	public static final String MOD_NAME = "Taam";
 	public static final String MOD_VERSION = "0.1";
+	public static final String MOD_AUTHOR = "founderio";
+	public static final String MOD_DESCRIPTION = "Tech and Acessories Mod";
+	public static final String MOD_CREDITS = "Special thanks to XYZ";
+	public static final String MOD_LOGO_PATH = "";
 
 	public static final String BLOCK_SENSOR = "taam.sensor";
 	public static final String BLOCK_SENSOR_MOTION = "taam.sensor.motion";
