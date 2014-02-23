04c93f8206679d1f9dcd9fa9a43b8109846ab048
founderio <oliver.kahrmann@gmail.com>
2014-02-23 19:23:55 +0100

    Changed Authors-Meta, TODO-CHanges

--- common/founderio/taam/Taam.java
+++ common/founderio/taam/Taam.java
@@ -8,9 +8,10 @@ public final class Taam {
 	public static final String MOD_ID = "taam";
 	public static final String MOD_NAME = "Taam";
 	public static final String MOD_VERSION = "0.1";
-	public static final String MOD_AUTHOR = "founderio";
+	public static final String MOD_AUTHOR1 = "founderio";
+	public static final String MOD_AUTHOR2 = "Xander112";
 	public static final String MOD_DESCRIPTION = "Tech and Acessories Mod";
-	public static final String MOD_CREDITS = "Special thanks to Xander112";
+	public static final String MOD_CREDITS = "";
 	public static final String MOD_LOGO_PATH = "";
 
 	public static final String BLOCK_SENSOR = "taam.sensor";
