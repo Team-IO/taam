359448296486c5db2eecdbd17ee938c32ca334fc
founderio <oliver.kahrmann@gmail.com>
2014-02-28 16:30:34 +0100

    Cables can now be hit

--- common/founderio/taam/blocks/multinet/MultinetCable.java
+++ common/founderio/taam/blocks/multinet/MultinetCable.java
@@ -1,17 +1,30 @@
 package founderio.taam.blocks.multinet;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraftforge.common.ForgeDirection;
 import codechicken.lib.data.MCDataInput;
 import codechicken.lib.data.MCDataOutput;
 import codechicken.lib.lighting.LazyLightMatrix;
+import codechicken.lib.raytracer.ExtendedMOP;
+import codechicken.lib.raytracer.IndexedCuboid6;
 import codechicken.lib.render.CCModel;
 import codechicken.lib.vec.BlockCoord;
+import codechicken.lib.vec.Cuboid6;
 import codechicken.lib.vec.Vector3;
 import codechicken.multipart.TMultiPart;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import founderio.taam.Taam;
+import founderio.taam.TaamMain;
 
 public class MultinetCable extends TMultiPart {
 
@@ -213,6 +226,176 @@ public class MultinetCable extends TMultiPart {
 		default:
 			break;
 		}
+	}
+	
+	@Override
+	public Iterable<Cuboid6> getCollisionBoxes() {
+		List<Cuboid6> boxes = new ArrayList<Cuboid6>();
+		ForgeDirection dir = ForgeDirection.getOrientation(face);
+
+		float layerOffset = (float)layer/(float)layerCount;
+		float ox1 = 0;
+		float oy1 = 0;
+		float oz1 = 0;
+		float ox2 = 0;
+		float oy2 = 0;
+		float oz2 = 0;
+		
+		
+		switch(dir) {
+		case DOWN:
+			ox1 = layerOffset;
+			oy1 = 0;
+			oz1 = 0;
+			ox2 = layerOffset + cableWidth;
+			oy2 = cableWidth;
+			oz2 = 1;
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+								  ox2, oy2, oz2));
+			ox1 = 0;
+			oy1 = 0;
+			oz1 = layerOffset;
+			ox2 = 1;
+			oy2 = cableWidth;
+			oz2 = layerOffset + cableWidth;
+
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			break;
+		case UP:
+			ox1 = layerOffset;
+			oy1 = 1 - cableWidth;
+			oz1 = 0;
+			ox2 = layerOffset + cableWidth;
+			oy2 = 1;
+			oz2 = 1;
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			ox1 = 0;
+			oy1 = 1f - cableWidth;
+			oz1 = layerOffset;
+			ox2 = 1;
+			oy2 = 1;
+			oz2 = layerOffset + cableWidth;
+
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			break;
+		case NORTH:
+			ox1 = layerOffset;
+			oy1 = 0;
+			oz1 = 0;
+			ox2 = layerOffset + cableWidth;
+			oy2 = 1;
+			oz2 = cableWidth;
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			ox1 = 0;
+			oy1 = layerOffset;
+			oz1 = 0;
+			ox2 = 1;
+			oy2 = layerOffset + cableWidth;
+			oz2 = cableWidth;
+
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			break;
+		case SOUTH:
+			ox1 = layerOffset;
+			oy1 = 0;
+			oz1 = 1 - cableWidth;
+			ox2 = layerOffset + cableWidth;
+			oy2 = 1;
+			oz2 = 1;
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			ox1 = 0;
+			oy1 = layerOffset;
+			oz1 = 1 - cableWidth;
+			ox2 = 1;
+			oy2 = layerOffset + cableWidth;
+			oz2 = 1;
+
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			break;
+		case WEST:
+			ox1 = 0;
+			oy1 = 0;
+			oz1 = layerOffset;
+			ox2 = cableWidth;
+			oy2 = 1;
+			oz2 = layerOffset + cableWidth;
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			ox1 = 0;
+			oy1 = layerOffset;
+			oz1 = 0;
+			ox2 = cableWidth;
+			oy2 = layerOffset + cableWidth;
+			oz2 = 1;
+
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			break;
+		case EAST:
+			ox1 = 1 - cableWidth;
+			oy1 = 0;
+			oz1 = layerOffset;
+			ox2 = 1;
+			oy2 = 1;
+			oz2 = layerOffset + cableWidth;
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			ox1 = 1 - cableWidth;
+			oy1 = layerOffset;
+			oz1 = 0;
+			ox2 = 1;
+			oy2 = layerOffset + cableWidth;
+			oz2 = 1;
+
+			boxes.add(new Cuboid6(ox1, oy1, oz1,
+					  ox2, oy2, oz2));
+			break;
+		default:
+			break;
+		}
+		return boxes;
+	}
+	
+	@Override
+	public Iterable<ItemStack> getDrops() {
+		List<ItemStack> drops = new ArrayList<ItemStack>();
+		drops.add(new ItemStack(TaamMain.itemMultinetCable, 1, ItemMultinetCable.cables.indexOf(part)));
+		return drops;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean drawHighlight(MovingObjectPosition hit, EntityPlayer player,
+			float frame) {
+
+        GL11.glBegin(GL11.GL_LINES);
+        GL11.glPushMatrix();
+        GL11.glTranslated(0.5, 0.5, 0.5);
+        GL11.glScaled(1.002, 1.002, 1.002);
+        GL11.glTranslated(-0.5, -0.5, -0.5);
+		
+        MultinetCable.render(new Vector3(hit.blockX,  hit.blockY, hit.blockZ), null, 1, hit.sideHit, layer);
+
+        GL11.glPopMatrix();
+        GL11.glEnd();
+        return true;
+	}
+	
+	@Override
+	public Iterable<IndexedCuboid6> getSubParts() {
+		List<Cuboid6> boxes = (List<Cuboid6>) getCollisionBoxes();
+		List<IndexedCuboid6> retBoxes = new ArrayList<IndexedCuboid6>();
+		for(Cuboid6 box : boxes) {
+			retBoxes.add(new IndexedCuboid6(null, box));
+		}
+		return retBoxes;
 	}
 	
 	@Override
