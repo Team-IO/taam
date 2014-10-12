1b9cf85787e9c5d5c73d287489a47bfd73a1fc53
founderio <oliver.kahrmann@gmail.com>
2014-10-12 21:39:33 +0200

    Added Gui Config for other config categories

--- common/founderio/taam/client/gui/ModGuiConfig.java
+++ common/founderio/taam/client/gui/ModGuiConfig.java
@@ -1,9 +1,13 @@
 package founderio.taam.client.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 import cpw.mods.fml.client.config.GuiConfig;
+import cpw.mods.fml.client.config.IConfigElement;
 import founderio.taam.Config;
 import founderio.taam.Taam;
 
@@ -14,12 +18,26 @@ public class ModGuiConfig extends GuiConfig {
 	public ModGuiConfig(GuiScreen guiScreen)
 	{
 		super(guiScreen,
-		new ConfigElement<Object>(Config.config.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+		getElements(),
 		Taam.MOD_ID,
 		false,
 		false,
 		GuiConfig.getAbridgedConfigPath(Config.config.toString()));
 		
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private static List<IConfigElement> getElements() {
+		List<IConfigElement> elements = new ArrayList<IConfigElement>();
+		ConfigElement<Object> cfgEl = new ConfigElement<Object>(Config.config.getCategory(Configuration.CATEGORY_GENERAL));
+		ConfigElement<Object> cfgElMultitronix = new ConfigElement<Object>(Config.config.getCategory("multitronix"));
+		ConfigElement<Object> cfgElProductionline = new ConfigElement<Object>(Config.config.getCategory("production_line_appliances"));
+		ConfigElement<Object> cfgElWorldgen = new ConfigElement<Object>(Config.config.getCategory("worldgen"));
+		elements.addAll(cfgEl.getChildElements());
+		elements.addAll(cfgElMultitronix.getChildElements());
+		elements.addAll(cfgElProductionline.getChildElements());
+		elements.addAll(cfgElWorldgen.getChildElements());
+		return elements;
 	}
 
 }
