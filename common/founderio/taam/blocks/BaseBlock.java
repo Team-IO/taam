1e32ffcebd0ad0c8d90f0be592d3eb8f2ce87f1c
founderio <oliver.kahrmann@gmail.com>
2014-10-31 22:26:51 +0100

    Made conveyor hopper drop its content on destruction

--- common/founderio/taam/blocks/BaseBlock.java
+++ common/founderio/taam/blocks/BaseBlock.java
@@ -4,8 +4,12 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import codechicken.lib.inventory.InventoryUtils;
+import codechicken.lib.vec.Vector3;
 
 public abstract class BaseBlock extends Block {
 
@@ -32,6 +36,45 @@ public abstract class BaseBlock extends Block {
 		world.notifyBlocksOfNeighborChange(x, y, z - 1, this);
 		world.notifyBlocksOfNeighborChange(x, y - 1, z, this);
 		world.notifyBlocksOfNeighborChange(x, y + 1, z, this);
+	}
+	
+	@Override
+	public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
+		if (world.isRemote) {
+			return;
+		}
+		TileEntity te = world.getTileEntity(x, y, z);
+
+		/*
+		 * Drop Items
+		 */
+		if(te instanceof IInventory) {
+			IInventory inventory = (IInventory)te;
+			for (int index = 0; index < inventory.getSizeInventory(); index++) {
+				ItemStack itemstack = inventory.getStackInSlot(index);
+
+				if (itemstack != null && itemstack.getItem() != null) {
+					InventoryUtils.dropItem(itemstack, world, new Vector3(x, y, z));
+				}
+			}
+		}
+		
+		//TODO: Drop stuff like conveyor appliances
+//		/*
+//		 * Drop Modules
+//		 */
+//		if(te instanceof IModuleHost) {
+//			IModuleHost moduleHost = (IModuleHost)te;
+//			for (int index = 0; index < moduleHost.getSizeModules(); index++) {
+//				ItemStack itemstack = moduleHost.getModuleItemStack(index);
+//
+//				if (itemstack != null && itemstack.getItem() != null) {
+//					ItemUtil.spawnItemStackDropped(itemstack, world, x, y, z);
+//				}
+//			}
+//		}
+
+		super.breakBlock(world, x, y, z, block, meta);
 	}
 
 }
