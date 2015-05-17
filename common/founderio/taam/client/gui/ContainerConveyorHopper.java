a6a7de066160d63f27f2ec91ab6012ee350cb03e
founderio <oliver.kahrmann@gmail.com>
2015-05-17 17:16:02 +0200

    Fixes #23 (should now behave like a vanilla hopper)

--- common/founderio/taam/client/gui/ContainerConveyorHopper.java
+++ common/founderio/taam/client/gui/ContainerConveyorHopper.java
@@ -39,37 +39,34 @@ public class ContainerConveyorHopper extends Container {
 	}
 
 	@Override
-	public ItemStack transferStackInSlot(EntityPlayer player, int slot) {
+	public ItemStack transferStackInSlot(EntityPlayer player, int slotID) {
 		ItemStack stack = null;
-		Slot slotObject = (Slot) inventorySlots.get(slot);
+		Slot slot = (Slot) inventorySlots.get(slotID);
 
 		// null checks and checks if the item can be stacked (maxStackSize > 1)
-		if (slotObject != null && slotObject.getHasStack()) {
-			ItemStack stackInSlot = slotObject.getStack();
+		if (slot != null && slot.getHasStack()) {
+			ItemStack stackInSlot = slot.getStack();
 			stack = stackInSlot.copy();
-
+			
 			// merges the item into player inventory since its in the tileEntity
-			if (slot < 9) {
-				if (!this.mergeItemStack(stackInSlot, 0, 35, true)) {
+			if (slotID < tileEntity.getSizeInventory()) {
+				if (!this.mergeItemStack(stackInSlot, tileEntity.getSizeInventory(), this.inventorySlots.size(), true)) {
 					return null;
 				}
 			}
-			// places it into the tileEntity is possible since its in the player
-			// inventory
-			else if (!this.mergeItemStack(stackInSlot, 0, 9, false)) {
+			// merge into tileEntity inventory, since it is in player's inventory
+			else if (!this.mergeItemStack(stackInSlot, 0, tileEntity.getSizeInventory(), false)) {
 				return null;
 			}
 
-			if (stackInSlot.stackSize == 0) {
-				slotObject.putStack(null);
-			} else {
-				slotObject.onSlotChanged();
-			}
-
-			if (stackInSlot.stackSize == stack.stackSize) {
-				return null;
-			}
-			slotObject.onPickupFromSlot(player, stackInSlot);
+			if (stackInSlot.stackSize == 0)
+            {
+                slot.putStack(null);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
 		}
 		return stack;
 	}
