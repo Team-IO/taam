0a4a139bf46469ce493bb00d3da285a7e9825b85
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2016-03-28 21:05:53 +0200

    Fix alpha rendering in tanks / pipe blocks

--- common/net/teamio/taam/content/piping/BlockPipeMachines.java
+++ common/net/teamio/taam/content/piping/BlockPipeMachines.java
@@ -11,6 +11,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumWorldBlockLayer;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -22,34 +23,39 @@ import net.teamio.taam.content.MaterialMachinesTransparent;
 public class BlockPipeMachines extends BaseBlock {
 
 	public static final PropertyEnum<Taam.BLOCK_PIPEMACHINES_META> VARIANT = PropertyEnum.create("variant", Taam.BLOCK_PIPEMACHINES_META.class);
-	
+
 	public BlockPipeMachines() {
 		super(MaterialMachinesTransparent.INSTANCE);
 		this.setHardness(3.5f);
 		this.setStepSound(Block.soundTypeMetal);
 		this.setHarvestLevel("pickaxe", 1);
 	}
-	
+
 	@Override
 	protected BlockState createBlockState() {
 		return new BlockState(this, VARIANT);
 	}
-	
+
 	@Override
 	public int getMetaFromState(IBlockState state) {
-		Taam.BLOCK_PIPEMACHINES_META meta = (Taam.BLOCK_PIPEMACHINES_META)state.getValue(VARIANT);
+		Taam.BLOCK_PIPEMACHINES_META meta = (Taam.BLOCK_PIPEMACHINES_META) state.getValue(VARIANT);
 		return meta.ordinal();
 	}
-	
+
 	@Override
 	public IBlockState getStateFromMeta(int meta) {
 		Taam.BLOCK_PIPEMACHINES_META[] values = Taam.BLOCK_PIPEMACHINES_META.values();
-		if(meta < 0 || meta > values.length) {
+		if (meta < 0 || meta > values.length) {
 			return getDefaultState();
 		}
 		return getDefaultState().withProperty(VARIANT, values[meta]);
 	}
-	
+
+	@SideOnly(Side.CLIENT)
+	public EnumWorldBlockLayer getBlockLayer() {
+		return EnumWorldBlockLayer.CUTOUT;
+	}
+
 	public String getUnlocalizedName(ItemStack itemStack) {
 		int i = itemStack.getItemDamage();
 		Enum<?>[] values = Taam.BLOCK_PIPEMACHINES_META.values();
@@ -60,7 +66,7 @@ public class BlockPipeMachines extends BaseBlock {
 
 		return super.getUnlocalizedName() + "." + values[i].name();
 	}
-	
+
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void getSubBlocks(Item item, CreativeTabs creativeTab, List<ItemStack> list) {
@@ -69,11 +75,11 @@ public class BlockPipeMachines extends BaseBlock {
 			list.add(new ItemStack(item, 1, i));
 		}
 	}
-	
+
 	@Override
 	public TileEntity createTileEntity(World world, IBlockState state) {
-		Taam.BLOCK_PIPEMACHINES_META variant = (Taam.BLOCK_PIPEMACHINES_META)state.getValue(VARIANT);
-		switch(variant) {
+		Taam.BLOCK_PIPEMACHINES_META variant = (Taam.BLOCK_PIPEMACHINES_META) state.getValue(VARIANT);
+		switch (variant) {
 		case tank:
 			return new TileEntityTank();
 		case creativewell:
@@ -82,7 +88,7 @@ public class BlockPipeMachines extends BaseBlock {
 		Log.error("Was not able to create a TileEntity for " + getClass().getName());
 		return null;
 	}
-	
+
 	@Override
 	public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state) {
 		return true;
