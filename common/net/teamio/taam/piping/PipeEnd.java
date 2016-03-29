445cc533f911ce9997980c2fb3ad1d6f65ed06d7
Alexander Sieg <Xander.lp112@gmail.com>
2016-03-29 21:42:45 +0200

    cleanup

--- common/net/teamio/taam/piping/PipeEnd.java
+++ common/net/teamio/taam/piping/PipeEnd.java
@@ -1,7 +1,6 @@
 package net.teamio.taam.piping;
 
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
