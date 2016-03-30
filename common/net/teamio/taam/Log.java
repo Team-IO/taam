81bbd70e34f20254a0740a830e9d0b055cfaab9b
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2016-03-30 16:20:03 +0200

    Add parametrized logging

--- common/net/teamio/taam/Log.java
+++ common/net/teamio/taam/Log.java
@@ -9,9 +9,17 @@ public final class Log {
 	public static void warn(String msg){
 		LOGGER.warn(msg);
 	}
+	
+	public static void warn(String msg, Object... params){
+		LOGGER.warn(msg, params);
+	}
 
 	public static void error(String msg){
 		LOGGER.error(msg);
+	}
+	
+	public static void error(String msg, Object... params){
+		LOGGER.error(msg, params);
 	}
 
 	public static void error(String msg, Throwable e){
@@ -22,11 +30,23 @@ public final class Log {
 		LOGGER.info(msg);
 	}
 	
+	public static void info(String msg, Object... params){
+		LOGGER.info(msg, params);
+	}
+	
 	public static void debug(String msg){
 		if(Config.debug) {
 			LOGGER.info(msg);
 		} else {
 			LOGGER.debug(msg);
+		}
+	}
+	
+	public static void debug(String msg, Object... params){
+		if(Config.debug) {
+			LOGGER.info(msg, params);
+		} else {
+			LOGGER.debug(msg, params);
 		}
 	}
 	
