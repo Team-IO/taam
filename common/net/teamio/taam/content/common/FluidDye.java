adabc5f7bc372149557cac0f63d08efd9dcadaa8
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2016-03-31 02:04:11 +0200

    Fix a crash with fluid dyes

--- common/net/teamio/taam/content/common/FluidDye.java
+++ common/net/teamio/taam/content/common/FluidDye.java
@@ -1,11 +1,12 @@
 package net.teamio.taam.content.common;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
 
 public class FluidDye extends Fluid {
 
 	public FluidDye(String fluidName) {
-		super(fluidName, null, null);//TODO: ResourceLocations
+		super(fluidName, new ResourceLocation("taam", "dye"), new ResourceLocation("taam", "dye"));//TODO: ResourceLocations
 		setViscosity(400);
 		setDensity(1300);
 	}
