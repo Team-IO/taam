c09f674654c160557ec315ce4a934b9814157c76
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2016-05-07 20:02:35 +0200

    Fix machines getting destroyed by fluids

--- common/net/teamio/taam/content/MaterialMachinesTransparent.java
+++ common/net/teamio/taam/content/MaterialMachinesTransparent.java
@@ -25,7 +25,7 @@ public class MaterialMachinesTransparent extends Material {
     @Override
     public boolean blocksMovement()
     {
-        return false;
+        return true;
     }
     
     @Override
