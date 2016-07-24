14854781bcd3fe749854b88fd7d83357702960f8
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2016-07-24 22:03:17 +0200

    Add option to switch off debug output completely

--- common/net/teamio/taam/Log.java
+++ common/net/teamio/taam/Log.java
@@ -43,8 +43,44 @@ public final class Log {
 		}
 	}
 
+	public static void debug(String msg, Object a){
+		if(!Config.debug_output) {
+			return;
+		}
+		if(Config.debug_output_as_info) {
+			LOGGER.info(msg, a);
+		} else {
+			LOGGER.debug(msg, a);
+		}
+	}
+
+	public static void debug(String msg, Object a, Object b){
+		if(!Config.debug_output) {
+			return;
+		}
+		if(Config.debug_output_as_info) {
+			LOGGER.info(msg, a, b);
+		} else {
+			LOGGER.debug(msg, a, b);
+		}
+	}
+
+	public static void debug(String msg, Object a, Object b, Object c){
+		if(!Config.debug_output) {
+			return;
+		}
+		if(Config.debug_output_as_info) {
+			LOGGER.info(msg, a, b, c);
+		} else {
+			LOGGER.debug(msg, a, b, c);
+		}
+	}
+
 	public static void debug(String msg, Object... params){
-		if(Config.debug_output) {
+		if(!Config.debug_output) {
+			return;
+		}
+		if(Config.debug_output_as_info) {
 			LOGGER.info(msg, params);
 		} else {
 			LOGGER.debug(msg, params);
