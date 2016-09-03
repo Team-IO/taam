28bdd6d4a626947e2fb1891abc4ddaf70ab6cac3
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2016-09-03 12:56:08 +0200

    Add grinder recipes for flowers to dyes (Fixes #123)

--- common/net/teamio/taam/TaamRecipes.java
+++ common/net/teamio/taam/TaamRecipes.java
@@ -230,6 +230,93 @@ public class TaamRecipes {
 					new ChancedOutput(new ItemStack(Items.DYE, 1, 15-col), 0.001f )
 					));
 		}
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.YELLOW_FLOWER),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 11), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 11), 0.4f)
+		));
+		/*
+		0 	Dandelion
+		 */
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.RED_FLOWER),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 1), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 1), 0.4f)
+		));
+		/*
+
+
+
+
+		 */
+		//0 	Poppy
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.RED_FLOWER, 1, 0),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 1), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 1), 0.4f)
+		));
+		//1 	Blue Orchid
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.RED_FLOWER, 1, 1),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 12), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 12), 0.4f)
+		));
+		//2 	Allium
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.RED_FLOWER, 1, 2),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 13), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 13), 0.4f)
+		));
+		//3 	Azure Bluet
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.RED_FLOWER, 1, 3),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 7), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 7), 0.4f)
+		));
+		//4 	Red Tulip
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.RED_FLOWER, 1, 4),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 1), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 1), 0.4f)
+		));
+		//5 	Orange Tulip
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.RED_FLOWER, 1, 5),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 14), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 14), 0.4f)
+		));
+		//6 	White Tulip
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.RED_FLOWER, 1, 6),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 7), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 7), 0.4f)
+		));
+		//7 	Pink Tulip
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.RED_FLOWER, 1, 7),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 9), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 9), 0.4f)
+		));
+		//8 	Oxeye Daisy
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.RED_FLOWER, 1, 8),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 7), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 7), 0.4f)
+		));
+		/*
+		Tall flowers
+		 */
+		//0 	Sunflower
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.DOUBLE_PLANT, 1, 0),
+				new ChancedOutput(new ItemStack(Items.DYE, 2, 11), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 11), 0.4f)
+		));
+		//1 	Lilac
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.DOUBLE_PLANT, 1, 1),
+				new ChancedOutput(new ItemStack(Items.DYE, 2, 13), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 13), 0.4f)
+		));
+		//2 	Double Tallgrass
+		//3 	Large Fern
+		//4 	Rose Bush
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.DOUBLE_PLANT, 1, 4),
+				new ChancedOutput(new ItemStack(Items.DYE, 2, 1), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 1), 0.4f)
+		));
+		//5 	Peony
+		ProcessingRegistry.registerRecipe(ProcessingRegistry.GRINDER, new GrinderRecipe(new ItemStack(Blocks.DOUBLE_PLANT, 1, 5),
+				new ChancedOutput(new ItemStack(Items.DYE, 2, 9), 1f),
+				new ChancedOutput(new ItemStack(Items.DYE, 1, 9), 0.4f)
+		));
 
 		/*
 		 * Mixer
