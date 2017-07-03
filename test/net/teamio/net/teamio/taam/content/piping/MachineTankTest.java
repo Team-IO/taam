3e1448f2edf6a312af4e3cdd57e88b6d1cdb652c
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2017-07-03 22:58:04 +0200

    More tests for MachineTank

--- test/net/teamio/net/teamio/taam/content/piping/MachineTankTest.java
+++ test/net/teamio/net/teamio/taam/content/piping/MachineTankTest.java
@@ -1,5 +1,6 @@
 package net.teamio.net.teamio.taam.content.piping;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
@@ -23,39 +24,91 @@ public class MachineTankTest {
 
 		IFluidHandler fluidHandler = tank.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, EnumFacing.UP);
 		IPipe pipe = tank.getCapability(Taam.CAPABILITY_PIPE, EnumFacing.UP);
+		IPipe pipe2 = tank.getCapability(Taam.CAPABILITY_PIPE, EnumFacing.DOWN);
 
 		t.assertNotNull(fluidHandler);
 		t.assertNull(fluidHandler.drain(1, false));
 
+		// Insert in FH
 		int amount = fluidHandler.fill(new FluidStack(FluidRegistry.WATER, 5), true);
 		t.assertEquals(5, amount);
 
+		// Check amount from the pipes
 		amount = pipe.getFluidAmount(new FluidStack(FluidRegistry.WATER, 0));
 		t.assertEquals(5, amount);
+		amount = pipe2.getFluidAmount(new FluidStack(FluidRegistry.WATER, 0));
+		t.assertEquals(5, amount);
 
+		// Drain from FH, fluid not in tank
 		FluidStack drained = fluidHandler.drain(new FluidStack(FluidRegistry.LAVA, 5), true);
 		t.assertNull(drained);
 
+		// Check amount from the pipes
 		amount = pipe.getFluidAmount(new FluidStack(FluidRegistry.WATER, 0));
 		t.assertEquals(5, amount);
 		amount = pipe.getFluidAmount(new FluidStack(FluidRegistry.LAVA, 0));
 		t.assertEquals(0, amount);
+		amount = pipe2.getFluidAmount(new FluidStack(FluidRegistry.WATER, 0));
+		t.assertEquals(5, amount);
+		amount = pipe2.getFluidAmount(new FluidStack(FluidRegistry.LAVA, 0));
+		t.assertEquals(0, amount);
 
+		// Overdrain from FH
 		drained = fluidHandler.drain(new FluidStack(FluidRegistry.WATER, 20), true);
 		t.assertNotNull(drained);
 		t.assertEquals(5, drained.amount);
 
+		// Check amount from the pipes
 		amount = pipe.getFluidAmount(new FluidStack(FluidRegistry.WATER, 0));
 		t.assertEquals(0, amount);
 		amount = pipe.getFluidAmount(new FluidStack(FluidRegistry.LAVA, 0));
 		t.assertEquals(0, amount);
+		amount = pipe2.getFluidAmount(new FluidStack(FluidRegistry.WATER, 0));
+		t.assertEquals(0, amount);
+		amount = pipe2.getFluidAmount(new FluidStack(FluidRegistry.LAVA, 0));
+		t.assertEquals(0, amount);
 
+		// Insert from pipe
 		pipe.addFluid(new FluidStack(FluidRegistry.WATER, 5));
 
+		// Drain from FH
 		drained = fluidHandler.drain(new FluidStack(FluidRegistry.WATER, 5), false);
 		t.assertNotNull(drained);
 		t.assertEquals(5, drained.amount);
 
+
+	}
+
+	@TestMethod
+	public void testSerializeTankContent(TestingHarness t) {
+
+		MachineTank tank = new MachineTank();
+		IPipe pipe = tank.getCapability(Taam.CAPABILITY_PIPE, EnumFacing.UP);
+
+		int amount = pipe.addFluid(new FluidStack(FluidRegistry.WATER, 315));
+		t.assertEquals(315, amount);
+		amount = pipe.getFluidAmount(new FluidStack(FluidRegistry.WATER, 0));
+		t.assertEquals(315, amount);
+
+		// Write to NBT
+		NBTTagCompound tag = new NBTTagCompound();
+		tank.writePropertiesToNBT(tag);
+
+		// Read into same tank
+		tank.readPropertiesFromNBT(tag);
+
+		// Check amount
+		amount = pipe.getFluidAmount(new FluidStack(FluidRegistry.WATER, 0));
+		t.assertEquals(315, amount);
+
+		// Read into new tank
+		tank = new MachineTank();
+		pipe = tank.getCapability(Taam.CAPABILITY_PIPE, EnumFacing.UP);
+		tank.readPropertiesFromNBT(tag);
+
+		// Check amount
+		amount = pipe.getFluidAmount(new FluidStack(FluidRegistry.WATER, 0));
+		t.assertEquals(315, amount);
 	}
 
 	@TestMethod
